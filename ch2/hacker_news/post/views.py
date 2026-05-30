882b744d3e9f907ed02f4e502f456f87d368aed3
allkong <allempti@gmail.com>
2026-05-31 06:36:18 +0900

    Section 3: 42. Class-based View

--- ch2/hacker_news/post/views.py
+++ ch2/hacker_news/post/views.py
@@ -1,24 +1,29 @@
+from urllib import request
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import F
+from django.views import View
 
 from post.models import Post
 from post.forms import PostForm
 
-def posts_view(request):
-    if request.method == "GET":
+
+class PostListView(View):
+    def get(self, request):
         posts = Post.objects.all()
         context = {"posts": posts, "form": PostForm}
         return render(
             request, template_name="post_list.html", context=context
         )
 
-def post_create_view(request):
-    if request.method == "GET":
+class PostCreateView(View):
+    def get(self, request):
         context = {"form": PostForm}
         return render(
             request, template_name="post_create.html", context=context
         )
-    elif request.method == "POST":
+
+    def post(self, request):
         form = PostForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data["title"]
@@ -35,18 +40,20 @@ def post_create_view(request):
     
         return redirect("posts")
 
-def post_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    context = {"post": post}
-    return render(
-        request, template_name="post_detail.html", context=context
-    )
+class PostDetailView(View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        context = {"post": post}
+        return render(
+            request, template_name="post_detail.html", context=context
+        )
 
-def post_like_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    post.points = F("points") + 1 # post.points = F()
-    post.save()
-    post.refresh_from_db()
-    return render(
-        request, template_name="post_detail.html", context={"post": post}
-    )
+class PostLikeView(View):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        post.points = F("points") + 1 # post.points = F()
+        post.save()
+        post.refresh_from_db()
+        return render(
+            request, template_name="post_detail.html", context={"post": post}
+        )
