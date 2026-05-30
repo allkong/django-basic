6cbba11443eae1718e8c80d5fa4dcc067e283c39
allkong <allempti@gmail.com>
2026-05-31 05:50:38 +0900

    feat: Section 3.35 Djago Form

--- ch2/hacker_news/post/views.py
+++ ch2/hacker_news/post/views.py
@@ -1,29 +1,34 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404, redirect
 from post.models import Post
+from post.forms import PostForm
 
 def posts_view(request):
     if request.method == "GET":
         posts = Post.objects.all()
-        context = {"posts": posts}
-        return render(request, template_name="post_list.html", context=context)
+        context = {"posts": posts, "form": PostForm}
+        return render(
+            request, template_name="post_list.html", context=context
+        )
     elif request.method == "POST":
-        title = request.POST.get("title")
-        body = request.POST.get("body")
-        author_name = request.POST.get("author_name")
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            author_name = form.cleaned_data["author_name"]
 
-        if not (type(title) is str and 0 < len(title) <= 128):
-            return HttpResponseBadRequest("Invalid title")
-        if not (type(body) is str and 0 < len(body) <= 1024):
-            return HttpResponseBadRequest("Invalid body")
-        if not (type(author_name) is str and 0 < len(author_name) <= 32):
-            return HttpResponseBadRequest("Invalid author_name")
-
-        post = Post.objects.create(title=title, body=body, author_name=author_name)
-        context = {"post": post}
-        return render(request, template_name="post_detail.html", context=context)
+            post = Post.objects.create(
+                title=title, body=body, author_name=author_name
+            )
+            context = {"post": post}
+            return render(
+                request, template_name="post_detail.html", context=context
+            )
+    
+        return redirect("posts")
 
 def post_view(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     context = {"post": post}
-    return render(request, template_name="post_detail.html", context=context)
+    return render(
+        request, template_name="post_detail.html", context=context
+    )
