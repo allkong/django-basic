bc6094451accc4475b66490625ad58cb734ee609
allkong <allempti@gmail.com>
2026-06-07 22:25:58 +0900

    Section4: 52. 회원가입

--- ch3/auth_user/user/views.py
+++ ch3/auth_user/user/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 from django.views import View
+from user.forms import CustomUserCreationForm
 
 class HomeView(View):
     def get(self, request):
         # context = {"user": user, "perm": perms}
-        return render(request, 'home.html')
+        return render(request, "home.html")
+
+
+class SignUpView(View):
+    def get(self, request):
+        return render(
+            request, "registration/sign_up.html",
+            {"form": CustomUserCreationForm}
+        )
+    
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # session login
+        return redirect("home")
